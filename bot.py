1e19aeb54ebff5b26ee16ec818e765efbe50f791
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-09 22:57:01 +0300

    rec

--- bot.py
+++ bot.py
@@ -5,13 +5,15 @@ import parser
 TOKEN = "540973546:AAHkrTMvWclty8NSkCsQ1Bc5WqIiOUgvEUw"
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['record'])
+def record_handler(message):
+    bot.send_message(message.chat.id, 'Забронировать игру ')
+bot.polling()
+
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     bot.send_message(message.chat.id, 'Привет,я чат-бот клуба виртуальной реальности ')
 bot.polling()
 
-@bot.message_handler(commands=['record'])
-def record_handler(message):
-    bot.send_message(message.chat.id, 'Забронировать игру ')
-bot.polling()
+
 
