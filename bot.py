c7bf37fa4b7235b4407896b16069e99c59bc2f72
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-09 17:42:31 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import parser
+
+#main variables
+TOKEN = "540973546:AAHkrTMvWclty8NSkCsQ1Bc5WqIiOUgvEUw"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет,я чат-бот клуба виsdsdртуальной реальности ')
+bot.polling()
