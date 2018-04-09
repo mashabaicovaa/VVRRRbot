be0af96c398f41f37931d1299c52dc3c5c56457f
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-09 19:12:38 +0300

    g

--- bot.py
+++ bot.py
@@ -15,16 +15,3 @@ def start_handler(message):
     bot.send_message(message.chat.id, 'Забронировать игру ')
 bot.polling()
 
-@bot.message_handler(commands = ['url'])
-def url(message):
-    markup = types.InlineKeyboardMarkup()
-    btn_my_site= types.InlineKeyboardButton(text='Наш сайт', url='https://vr.gameofmind.ru/')
-    markup.add(btn_my_site)
-    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
-
-@bot.message_handler(content_types=['text'])
-def default_test(message):
-    keyboard = types.InlineKeyboardMarkup()
-    url_button = types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
-    keyboard.add(url_button)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди в поисковик.", reply_markup=keyboard)
