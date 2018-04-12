58785c8d2aaf2afeaec3b74754ad361bd6a53b16
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-12 17:53:53 +0300

    'name'

--- markups.py
+++ markups.py
@@ -4,7 +4,7 @@ start_markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 start_markup_btn1 = types.KeyboardButton('Запустить')
 start_markup.add(start_markup_btn1)
 
-menu_markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+menu_markup = types.ReplyKeyboardMarkup(row_width=3, width='25', resize_keyboard=True)
 menu_markup_btn1 = types.KeyboardButton('Игры')
 menu_markup_btn2 = types.KeyboardButton('Цены')
 menu_markup_btn3 = types.KeyboardButton('Где мы?')
