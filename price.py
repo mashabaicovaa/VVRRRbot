b705c1eb2dfed6adedefd9e888b027dca9a74e10
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-12 16:01:03 +0300

    'name'

--- price.py
+++ price.py
@@ -1,6 +1,6 @@
 class price():
     isRunning = False
-    price = ['20 минут -300 р\n40 минут-500р\n60 минут- 700р ']
+    all_price = ['20 минут -300 р\n40 минут-500р\n60 минут- 700р ']
     
 
     mySource = ''
