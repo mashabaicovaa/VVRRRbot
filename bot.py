11621ae7c811bb811f2ebd27be4316ab6f26adc8
Masha Baikova <mashabaicovaa@icloud.com>
2018-04-12 15:41:36 +0300

    'name'

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def main_menu(message):
         msg = bot.send_message(chat_id, 'Выберите категорию игр', reply_markup=m.catalog_markup)
         bot.register_next_step_handler(msg, game_catalog)
     elif text in task.names[1]:
-        msg = bot.send_message(chat_id, 'Цены', reply_markup=m.menu_markup)
+        msg = bot.send_message(chat_id, price.price, reply_markup=m.catalog_markup_btn6)
         bot.register_next_step_handler(msg, price)
     else:
         msg = bot.send_message(chat_id, 'Где мы находимся ?')
